d2cbfd4c17c72c37defa0eeaef0942af1440045d
Luke <43494998+arsity@users.noreply.github.com>
2023-01-11 23:38:35 +0800

    finish coding

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include "iostream"
 
 #include "eigen3/Eigen/Core"
-#include "eigen3/Eigen/Dense"
 #include "eigen3/Eigen/SVD"
 #include "eigen3/Eigen/Geometry"
 
 #include "opencv4/opencv2/core.hpp"
 #include "opencv4/opencv2/core/eigen.hpp"
 #include "opencv4/opencv2/imgcodecs.hpp"
-#include "opencv4/opencv2/highgui.hpp"
 
 #define GRAYSCALE_MAX 255
+#define LENNA_PATH "/run/media/luke/Data/CSC4140/CSC4140_Proj1/lenna.png"
+#define IMG1 "/run/media/luke/Data/CSC4140/CSC4140_Proj1/120090645_HW_01.assets/res1.png"
+#define IMG2 "/run/media/luke/Data/CSC4140/CSC4140_Proj1/120090645_HW_01.assets/res2.png"
+#define IMG3 "/run/media/luke/Data/CSC4140/CSC4140_Proj1/120090645_HW_01.assets/res3.png"
 
 using namespace std;
 class Op {
@@ -38,9 +40,9 @@ class Op {
 
 using namespace cv;
 class opencv {
-  std::string image_path = samples::findFile("/run/media/luke/Data/CSC4140/CSC4140_Proj1/lenna.png");
+  std::string image_path = samples::findFile(LENNA_PATH);
   Mat img = imread(image_path, IMREAD_GRAYSCALE);
-  Eigen::MatrixXf matrix;
+  Eigen::MatrixXd matrix;
 
  public:
   opencv() {
@@ -49,11 +51,50 @@ class opencv {
   }
 
   void res() {
-    Eigen::BDCSVD<Eigen::MatrixXf> bdcsvd(matrix);
+    Eigen::BDCSVD<Eigen::MatrixXd> bdcsvd(matrix, Eigen::ComputeFullU | Eigen::ComputeFullV);
     auto U = bdcsvd.matrixU();
+    auto singularValues = bdcsvd.singularValues();
     auto V = bdcsvd.matrixV();
-    auto S = bdcsvd.singularValues();
 
+    Mat resImg1, resImg2, resImg3;
+
+    Eigen::MatrixXd S1(512, 1);
+    for (auto i = 0; i < 512; ++i) {
+      S1(i, 0) = 0;
+    }
+    S1(0, 0) = singularValues(0);
+
+    Eigen::MatrixXd res1 = U * S1 * V.leftCols(1).transpose() * GRAYSCALE_MAX;
+    eigen2cv(res1, resImg1);
+    imwrite(IMG1, resImg1);
+
+    Eigen::MatrixXd S2(512, 10);
+    for (auto i = 0; i < 512; ++i) {
+      for (auto j = 0; j < 10; ++j) {
+        if (i == j) {
+          S2(i, j) = singularValues(i);
+        } else {
+          S2(i, j) = 0;
+        }
+      }
+    }
+    Eigen::MatrixXd res2 = U * S2 * V.leftCols(10).transpose() * GRAYSCALE_MAX;
+    eigen2cv(res2, resImg2);
+    imwrite(IMG2, resImg2);
+
+    Eigen::MatrixXd S3(512, 50);
+    for (auto i = 0; i < 512; ++i) {
+      for (auto j = 0; j < 50; ++j) {
+        if (i == j) {
+          S3(i, j) = singularValues(i);
+        } else {
+          S3(i, j) = 0;
+        }
+      }
+    }
+    Eigen::MatrixXd res3 = U * S3 * V.leftCols(50).transpose() * GRAYSCALE_MAX;
+    eigen2cv(res3, resImg3);
+    imwrite(IMG3, resImg3);
   }
 };
 
@@ -75,15 +116,15 @@ class trans {
 };
 
 int main() {
-  //  Op res{};
-  //  res.res1();
-  //  res.res2();
+  Op res{};
+  res.res1();
+  res.res2();
 
-  //  opencv oc{};
-  //  oc.res();
+  opencv oc{};
+  oc.res();
 
-//  trans trans{};
-//  trans.res();
+  trans trans{};
+  trans.res();
 
   return 0;
 }
